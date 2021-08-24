c775d651f02956d659de907facd90803aca8a87e
Yang Liu <jeremyrobturtle@gmail.com>
2021-08-24 21:25:12 +0800

    int_quatifier for Duration

---
+++ lib/int_quatifier.dart
@@ -0,0 +1,14 @@
+extension IntQuatifier on int {
+  Duration day() => Duration(days: this);
+  Duration days() => Duration(days: this);
+  Duration hour() => Duration(hours: this);
+  Duration hours() => Duration(hours: this);
+  Duration minute() => Duration(minutes: this);
+  Duration minutes() => Duration(minutes: this);
+  Duration second() => Duration(seconds: this);
+  Duration seconds() => Duration(seconds: this);
+  Duration microsecond() => Duration(microseconds: this);
+  Duration microseconds() => Duration(microseconds: this);
+  Duration millisecond() => Duration(milliseconds: this);
+  Duration milliseconds() => Duration(milliseconds: this);
+}
